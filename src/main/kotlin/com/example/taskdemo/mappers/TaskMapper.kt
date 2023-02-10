aadb6be81be92c89ec594d1ce68105ee17135312
barca <xbarca04@stud.fit.vutbr.cz>
2023-02-10 17:43:44 +0100

    CP

--- src/main/kotlin/com/example/taskdemo/mappers/TaskMapper.kt
+++ src/main/kotlin/com/example/taskdemo/mappers/TaskMapper.kt
@@ -4,18 +4,12 @@ import com.example.taskdemo.model.Task
 import com.example.taskdemo.model.entities.TaskEntity
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
-import org.mapstruct.Named
 
 @Mapper(componentModel = "spring")
 interface TaskMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "taskLockEntity", ignore = true)
-    @Mapping(source = "task", target = "clazz", qualifiedByName = ["taskToClazz"])
+    @Mapping(target = "clazz", expression = "java(task.getClass().getName())")
     fun toEntity(task: Task): TaskEntity
-
-    @Named("taskToClazz")
-    fun taskToClazz(task: Task): String {
-        return task::class.java.name //TODO: FIXME
-    }
 }
